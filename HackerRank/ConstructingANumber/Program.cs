5d7698e02a60fe7c5b6943ca434c01fa78410724
Peter Charij <CHARIJPX@legal.regn.net>
2021-11-12 12:23:44 +0000

    Additional Challenges

---
+++ HackerRank/ConstructingANumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConstructingANumber
+{
+    using System;
+    using System.Linq;
+
+    class Solution
+    {
+        public static void MainRun()
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                var s = Console.ReadLine().Sum(i => char.IsDigit(i) ? i - '0' : 0);
+
+                Console.WriteLine(s % 3 == 0
+                    ? "Yes"
+                    : "No");
+            }
+        }
+    }
+}
