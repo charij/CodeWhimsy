37f3f18d2f72db648ee56b3306aa3980a2477ae4
Peter Charij <pcharij@gmail.com>
2021-11-14 17:20:20 +0000

    climbing the tree

---
+++ HackerRank/ClimbingTheLeaderboard/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ClimbingTheLeaderboard
+{
+    using System;
+    using System.Linq;
+
+    class Solution
+    {
+        public static void MainRun()
+        {
+            Console.ReadLine();
+
+            var ranked = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).Distinct().ToArray();
+            var playerCount = int.Parse(Console.ReadLine());
+            var player = Array.ConvertAll(Console.ReadLine().Split(' '), i => int.Parse(i));
+
+            for (int i = 0, j = ranked.Length - 1; i < player.Length; i++)
+            {
+                while (j >= 0 && player[i] >= ranked[j]) j--;
+                Console.WriteLine(j + 2);
+            }
+        }
+    }
+}
---
+++ HackerRank/ProblemSolving/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ProblemSolving
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static string DecryptPassword(string s)
+        {
+            var stack = new Stack<char>();
+            for (int i = s.Length - 1, j = 0; i >= j; i--)
+            {
+                if (s[i] == '0')
+                {
+                    stack.Push(s[j++]);
+                }
+                else
+                if (s[i] == '*')
+                {
+                    stack.Push(s[i - 2]);
+                    stack.Push(s[i - 1]);
+                    i -= 2;
+                }
+                else
+                {
+                    stack.Push(s[i]);
+                }
+            }
+            return string.Join(string.Empty, stack);
+        }
+
+        public static List<int> StringAnagram(List<string> dictionary, List<string> query)
+        {
+            var lookUp = dictionary
+                .GroupBy(i => string.Join(string.Empty, i.OrderBy(i => i)))
+                .ToDictionary(i => i.Key, i => i.Count());
+
+            return query
+                .Select(i => string.Join(string.Empty, i.OrderBy(i => i)))
+                .Select(i => lookUp.ContainsKey(i) ? lookUp[i] : 0)
+                .ToList();
+        }
+    }
+}
